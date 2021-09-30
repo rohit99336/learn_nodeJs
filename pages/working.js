3bf7661ae76b148c1c3ed2daf0cf332dc23f8e15
mnjpatledivyal <manoj@divyaltech.com>
2021-09-30 09:26:23 +0530

    add expresss framwork in nodejs

---
+++ pages/working.js
@@ -0,0 +1,9 @@
+console.log("starting function");
+setTimeout(() => {
+  console.log("2 seccond");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 second");
+}, 0);
+console.log("function end ");
